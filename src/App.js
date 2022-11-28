280bf72d03c15ac9eb6a9ea961605e7d87c2e9b5
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2022-11-28 15:50:44 +0000

    Challenge ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
+import { useState,useEffect } from 'react';
+import { Button,Box } from '@chakra-ui/react';
+
 import './App.css';
 
+function Dot({coordinates}) {
+	const x = coordinates.x;
+	const y = coordinates.y;
+
+	return (
+		<Box position={"absolute"} left={x} top={y} className="dot" />
+	);
+}
+
 function App() {
+	const [globalDots, setGlobalDots] = useState([]);
+	const [dots, setDots] = useState([]);
+
+	useEffect(() => {
+		const header = document.querySelector('header');
+
+		const listener = window.addEventListener('click', (e) => {
+			if(e.target !== header ) return
+			const clientX = e.clientX;
+			const clientY = e.clientY;
+			setDots((dots) => [...dots, {x: clientX, y: clientY}]);
+			setGlobalDots((globalDots) => [...globalDots, {x: clientX, y: clientY}]);
+		});
+
+		return () => {
+			window.removeEventListener('click', listener);
+		}
+
+	}, []);
+
+	const handleUndo = () => {
+		if(dots.length === 0) return;
+		const currentLength = dots.length;
+		setDots((prev) => prev.slice(0,currentLength-1));
+	}
+
+	const handleRedo = () => {
+		if(dots.length === globalDots.length) return;
+		const currentLength = dots.length;
+		const c = globalDots[currentLength]
+		setDots((prev) => [...prev, c]);
+	}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+				<Button onClick={handleUndo} colorScheme="telegram" position={"absolute"} left={"10px"} top={"10px"} >Undo</Button>
+				<Button onClick={handleRedo} colorScheme="telegram" position={"absolute"} left={"100px"} top={"10px"} >Redo</Button>
+					{
+						dots.map((coordinates, index) => {
+							return <Dot coordinates={coordinates} key={index} />
+						})
+					}				
       </header>
     </div>
   );
